174e451f2828f739f92ece751821dbeaa09b894d
Михаил Драгункин <contact@md.land>
2025-05-05 21:52:07 +0300

    feature: separate library backend and frontend part versions

--- imohash_rs/__init__.py
+++ imohash_rs/__init__.py
@@ -1,13 +1,15 @@
 from .imohash_rs import (
-    __version__,
+    __version__ as __version_backend__,
     SAMPLE_THRESHOLD,
     SAMPLE_SIZE,
     Imohash,
     Hash,
 )
 
+__version__ = '0.1.0'
 __all__ = (
     '__version__',
+    '__version_backend__',
     'SAMPLE_THRESHOLD',
     'SAMPLE_SIZE',
     'Imohash',
